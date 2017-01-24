dd3617641e5e40a82511cac231eb8717cbfa0d60
Alec Grant <aleclouisgrant@gmail.com>
2017-01-24 14:18:54 -0500

    Added README, code formatted (80 char width)

--- rprocess.py
+++ rprocess.py
@@ -1,14 +1,17 @@
-# Preprocesses the reports from ZED, so that we can get it ready for use for the scheduler
+# Preprocesses the reports from ZED, so that we can get it ready for use for
+# the scheduler
 import csv
 import sys
 
-# DATA PREPROCESSING NEEDS TO 
+# DATA PREPROCESSING NEEDS TO
 # 1. Renames the title rows in the input csv
 #   Day Of Week -> weekday
 # 2. Removes the Requests, Location, Total Requests, and Total Hours column
-# 3. Turn each row of the preprocessed report into an object for use in the scheduler
+# 3. Turn each row of the preprocessed report into an object for use in the
+# scheduler
 
-# renames the columns, as well as removes the last three unnecessary rows from the report (requests, total requests, and total hours)
+# renames the columns, as well as removes the last three unnecessary rows
+# from the report (requests, total requests, and total hours)
 def processreport(filename, newfilename):
     with open(filename, "r") as csvFile, open(newfilename, "w") as newFile:
         reader = csv.reader(csvFile)
