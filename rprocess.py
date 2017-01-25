cb8b2f3c8dd5140e42ac67e1eea65f1bc2aeb99d
Alec Grant <aleclouisgrant@gmail.com>
2017-01-24 19:47:26 -0500

    Started scheduler.py. Formatted code (pls use 80 char width)

--- rprocess.py
+++ rprocess.py
@@ -1,12 +1,15 @@
-# Preprocesses the reports from Zed, so that we can get it ready for use for the scheduler
+# Preprocesses the reports from Zed, so that we can get it ready for use for
+# the scheduler
 # 1. Renames the title rows in the input csv
 #   Day Of Week -> weekday
 # 2. Removes the Requests, Location, Total Requests, and Total Hours column
-# And can turn each row of the preprocessed report into an object for use in the scheduler
+# And can turn each row of the preprocessed report into an object for use in
+# the scheduler
 import csv
 import cr
 
-# Renames the columns, as well as removes the last three unnecessary rows from the report (requests, total requests, and total hours)
+# Renames the columns, as well as removes the last three unnecessary rows from
+# the report (requests, total requests, and total hours)
 def processreport(filename, newfilename):
     with open(filename, "r") as csvFile, open(newfilename, "w") as newFile:
         reader = csv.reader(csvFile)
@@ -22,7 +25,8 @@ def processreport(filename, newfilename):
         for row in reader:
             writer.writerow((row[0], row[1], row[4], row[5]))
 
-# Returns a list of Shift objects based on an individual report created by processreport
+# Returns a list of Shift objects based on an individual report created by
+# processreport
 def loadshifts(filename):
     shifts = []
     with open(filename, 'r') as report:
@@ -37,10 +41,14 @@ def loadshifts(filename):
             shifts.append(shift)
     return shifts
 
-# Returns a tuple of two lists of Shift objects based on reports that has been processed by processreport()
-# 'supreport' should be the report that comes out after processing the shifts of sups
-# 'consreport' should be the report that comes out after processing the shifts of cons
-# USAGE:    allshifts = rprocess.loadreports('data/[sup report filename]', 'data/[cons report filename]')
+# Returns a tuple of two lists of Shift objects based on reports that has been
+# processed by processreport()
+# 'supreport' should be the report that comes out after processing the shifts
+# of sups
+# 'consreport' should be the report that comes out after processing the shifts
+# of cons
+# USAGE:    allshifts = rprocess.loadreports('data/[sup report filename]',
+# 'data/[cons report filename]')
 #           allshifts[0] # The list of all supervisor Shift objects
 #           allshifts[1] # The list of all consultant Shift objects
 #           allshifts[0][0] # The first Shift in the sup Shift list
